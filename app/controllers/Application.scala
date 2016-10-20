992f98e91d102927d53bef14641bb419cd460fee
micho <gatin13@yahoo.com>
2016-10-21 08:53:43 +0900

    Fixed bugs

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,10 +2,12 @@ package controllers
 
 import play.api.Play.current
 import play.api._
-import play.api.libs.oauth.{ConsumerKey, RequestToken}
+import play.api.libs.oauth.{ConsumerKey, OAuthCalculator, RequestToken}
+import play.api.libs.ws.WS
 import play.api.mvc._
 
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class Application extends Controller {
 
@@ -15,20 +17,31 @@ class Application extends Controller {
 
   // Uses "Action.async" to return a Future of a result for the next step
   def tweets = Action.async {
-    val credentials: Option[ (ConsumerKey, RequestToken) ] = for {
+    def credentials: Option[ (ConsumerKey, RequestToken) ] = for {
       // Retrieves the Twitter credentials from application.conf
       apiKey      <- Play.configuration.getString("twitter.apiKey")
       apiSecret   <- Play.configuration.getString("twitter.apiSecret")
       token       <- Play.configuration.getString("twitter.token")
       tokenSecret <- Play.configuration.getString("twitter.tokenSecret")
     } yield  (
-      ConsumerKey(apiKey, apiSecret)
+      ConsumerKey(apiKey, apiSecret),
       RequestToken(token, tokenSecret)
       )
 
-    credentials.map { case (ConsumerKey, RequestToken) =>
-      // Wraps the result in a successful Future block until the next step
-      Future.successful { Ok }
+    credentials.map { case (consumerKey, requestToken) =>
+      WS
+        // The API URL
+        .url("https://stream.twitter.com/1.1/statuses/filter.json")
+        // OAuth signature of the request
+        .sign(OAuthCalculator(consumerKey, requestToken))
+        // Specifies a query string parameter
+        .withQueryString("track" -> "reactive")
+        // Executes an HTTP GET request
+        .get()
+        .map {
+          response => Ok(response.body)
+        }
+
     } getOrElse {
       // Wraps the result in a successful Future block to comply with the return type
       Future.successful {
