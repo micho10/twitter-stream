33d9534f81bde29ba683a5462e0cba9c25f38b0c
micho <gatin13@yahoo.com>
2017-02-06 18:05:26 +0900

    Added failure management with a circuit breaker

--- app/actors/WebsocketClient.scala
+++ app/actors/WebsocketClient.scala
@@ -1,12 +1,12 @@
 package actors
 
 import akka.actor.{Actor, ActorRef, Props}
+import akka.pattern.CircuitBreaker
 import play.api.libs.oauth.{ConsumerKey, OAuthCalculator, RequestToken}
 import play.api.libs.ws.{WS, WSResponse}
 import play.api.{Logger, Play}
 
 import scala.concurrent.Future
-import scala.util.control.NonFatal
 
 /**
   * Created by carlos on 27/10/16.
@@ -15,14 +15,33 @@ class WebsocketClient(out: ActorRef) extends Actor {
 
   import play.api.Play.current
 
+  import scala.concurrent.duration._
+
   implicit val executionContext = context.dispatcher
+
+  var timeout = 1
+
+  val breaker = CircuitBreaker(
+    scheduler = context.system.scheduler,
+    maxFailures = 2,
+    callTimeout = 5.seconds,
+    resetTimeout = 5.seconds
+  ).onOpen {
+    Logger.error("Breaker open!!!!")
+  }.onHalfOpen {
+    Logger.warn("Breaker half open! Hang in there!")
+    timeout = 1000
+  }.onClose {
+    Logger.info("Breaker closed, yay!")
+  }
+
 
   override def receive = {
     case message: String =>
       Logger.info(s"Received message $message")
 
       credentials.map { case (consumerKey, requestToken) =>
-        val searchResult: Future[WSResponse] = WS
+        def searchResult: Future[WSResponse] = WS
           // The API URL
           .url("https://api.twitter.com/1.1/search/tweets.json")
           // Specifies a query string parameter
@@ -30,17 +49,24 @@ class WebsocketClient(out: ActorRef) extends Actor {
           // OAuth signature of the request
           .sign(OAuthCalculator(consumerKey, requestToken))
           /***** Set an unrealistic timeout to simulate Twitter being unavailable. *****/
-          .withRequestTimeout(1)
+          .withRequestTimeout(timeout)
           // Sends an HTTP GET request to the server and retrieves the response as a (possibly infinite) stream
           .get()
 
         import akka.pattern.pipe
 
-        searchResult.map { result =>
+        /***** Not needed with a circuit breaker *****/
+//        searchResult.map { result =>
+//          SearchResult(result.body)
+//        } recover { case NonFatal(t) =>
+//          SearchFailure(t)
+//        } pipeTo self
+
+        /***** Using a circuit breaker *****/
+        def mappedResult = searchResult.map { result =>
           SearchResult(result.body)
-        } recover { case NonFatal(t) =>
-          SearchFailure(t)
-        } pipeTo self
+        }
+          breaker.withCircuitBreaker(mappedResult) pipeTo self
       }
 
     case SearchResult(result) => out ! result
